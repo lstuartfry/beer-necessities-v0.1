775462ac519c3694d28918a2110d878ea2e1b2ac
Lukas Stuart-Fry <lstuartfry@gmail.com>
2020-03-23 17:18:23 -0700

    adds initial /client folder, babel, webpack, hot loading

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+import { hot } from 'react-hot-loader/root';
+import React from 'react';
+import './styles.css';
+
+const App = () => <div className="App">Hello world</div>;
+
+export default hot(App);
