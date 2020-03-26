2c7fcffb386f5a67786957260f8989b06848fe74
Lukas Stuart-Fry <lstuartfry@gmail.com>
2020-03-25 18:47:06 -0700

    adds Disclaimer component

---
+++ client/src/components/Disclaimer/Disclaimer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './styles';
+
+const Disclaimer = () => (
+	<div className={styles.disclaimer}>
+		Beer Necessities utilizes APIs from Youtube, Google Maps, Twitter, and the
+		BreweryDB to deliver a slew of cool videos and information about any beer or
+		brewery you're interested in.
+	</div>
+);
+
+export default Disclaimer;
--- client/src/components/MainContent/MainContent.jsx
+++ client/src/components/MainContent/MainContent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Disclaimer from '../Disclaimer/Disclaimer';
 import Landing from '../Landing/Landing';
 import Search from '../Search/Search';
 import styles from './styles';
@@ -8,6 +9,7 @@ const MainContent = () => (
 	<div className={styles.mainContent}>
 		<Landing />
 		<Search />
+		<Disclaimer />
 	</div>
 );
 
